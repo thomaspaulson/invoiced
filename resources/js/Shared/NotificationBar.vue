9ca2ee224921d00ca7bef5214ed4497aa83b3b7f
thomaspaulson <thomas.paulson@hotmail.com>
2020-05-06 14:32:39 +0530

    notification

---
+++ resources/js/Shared/NotificationBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="alert alert-success" role="alert" v-if="message.success">
+            {{ message.success }}
+        </div>
+        <div class="alert alert-danger" role="alert" v-if="message.error">
+            {{ message.error }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "notification-bar",
+        props: ['message']
+    }
+</script>
