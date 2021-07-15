5d1b27256b42a1f7a8acad0d578c1325b7d3c906
thomaspaulson <thomas.paulson@hotmail.com>
2021-07-15 23:24:59 +0530

    minor change in style

--- resources/views/layouts/leftmain.blade.php
+++ resources/views/layouts/leftmain.blade.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -33,12 +33,8 @@
     <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">        
     
 </head>
-<body id="app">
-    
-        <main class="py-4">
-            @yield('content')
-        </main>
-    
+<body id="app">            
+      @yield('content')    
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
